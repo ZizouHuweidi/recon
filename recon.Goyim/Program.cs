79b32558dfbac14da1049e96d730aa98febe0754
El Zubeir Huweidi <zubeir.huweidi@gmail.com>
2025-12-09 15:01:44 +0200

    chore: remove reference to old endpoint

--- recon.Goyim/Program.cs
+++ recon.Goyim/Program.cs
@@ -87,8 +87,3 @@ users.MapDelete("/{id}", async (int id, ReconDbContext db) =>
 app.MapDefaultEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
